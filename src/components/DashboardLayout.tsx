0c399a68dd4a20b3d751fb3fb86d28baeb9731e5
ismat27 <adegbiteismail27@gmail.com>
2023-05-09 17:01:51 +0100

    set up project layout

---
+++ src/components/DashboardLayout.tsx
@@ -0,0 +1,23 @@
+import { ReactNode, useState } from "react"
+import Sidebar from "./Sidebar"
+import HeaderDesktop from "./HeaderDesktop"
+import HeaderMobile from "./HeaderMobile"
+
+const DashboardLayout = ({ children }: {children: ReactNode}) => {
+    const [sideExpand, setSideExpand] = useState(true)
+    const [sideTranslated, setSideTranslated] = useState(true)
+    return (
+        <div className="bg-gray bg-gray-100 min-h-screen">
+            <Sidebar expand={sideExpand} setExpand={setSideExpand} sideTranslate={sideTranslated} />
+            <div className={`${sideExpand? 'lg:pl-[255px]': 'lg:pl-[88px]'}`}>
+                <HeaderMobile sideTranslated={sideTranslated} setSideTranslate={setSideTranslated}/>
+                <HeaderDesktop />
+                <main>
+                    { children }
+                </main>
+            </div>
+        </div>
+    )
+}
+
+export default DashboardLayout
