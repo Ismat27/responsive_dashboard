587d121e077f997dadcba7750080b98b04cdb53d
ismat27 <adegbiteismail27@gmail.com>
2023-05-09 17:05:04 +0100

    app pages

---
+++ src/pages/DashboardHome.tsx
@@ -0,0 +1,10 @@
+import DashboardLayout from "../components/DashboardLayout"
+const DashboardHome = () => {
+  return (
+    <DashboardLayout>
+        <div>DashboardHome</div>
+    </DashboardLayout>
+  )
+}
+
+export default DashboardHome
