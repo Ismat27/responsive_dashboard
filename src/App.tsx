26f0bc5b5c18308e7742455dfb829a75794622cb
ismat27 <adegbiteismail27@gmail.com>
2023-05-09 16:03:47 +0100

    render dashboard home

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,13 @@ import {
   createBrowserRouter, RouterProvider
 } from 'react-router-dom'
 import './App.css'
-
-const Home = () => {
-  return (
-    <div>
-      <h2 className='text-2xl text-red'>home page</h2>
-    </div>
-  )
-}
+import DashboardHome from './pages/DashboardHome'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />
+    element: <DashboardHome />
   },
-  {
-    path: '/abc',
-    element: <p>abc</p>
-  }
 ])
 
 function App() {
