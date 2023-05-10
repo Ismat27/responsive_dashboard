f5fc83615544a0925515c34e1c15f4df47211429
ismat27 <adegbiteismail27@gmail.com>
2023-05-10 17:59:28 +0100

    dummy data for promotions

---
+++ src/data/promotions.ts
@@ -0,0 +1,29 @@
+export const promotion_data = [
+  {
+    date_from: "28 Feb",
+    date_to: "07 Mar 22",
+    discount: "10",
+    reg_end: "22 Feb, 22",
+    title: "Lifestyle:  3.3 PAYDAY 2022    15% off min spend SGD 100   HOME10",
+    sellers: 3,
+    products: 4,
+  },
+  {
+    date_from: "28 Feb",
+    date_to: "07 Mar 22",
+    discount: "10",
+    reg_end: "22 Feb, 22",
+    title: "Lifestyle:  3.3 PAYDAY 2022    15% off min spend SGD 100   HOME10",
+    sellers: 0,
+    products: 1,
+  },
+  {
+    date_from: "28 Feb",
+    date_to: "07 Mar 22",
+    discount: "10",
+    reg_end: "22 Feb, 22",
+    title: "[CRM Campaign] Sports 30% + 10% CB (TGIF30)",
+    sellers: 1,
+    products: 2,
+  },
+];
