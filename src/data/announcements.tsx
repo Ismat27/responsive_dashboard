cdf0aaa70ef63d34e7681df347239f0711cb1375
ismat27 <adegbiteismail27@gmail.com>
2023-05-11 20:44:55 +0100

    dummy data for annnouncements

---
+++ src/data/announcements.tsx
@@ -0,0 +1,93 @@
+export type AnnouncementsDataProps = {
+  date: string;
+  title: string;
+  body: string;
+};
+
+export const announcements_data: AnnouncementsDataProps[] = [
+  {
+    date: "Feb 11",
+    title: "Zalora- Seller Communication Prioritization.",
+    body: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...",
+  },
+  {
+    date: "Feb 11",
+    title: "Zalora- Seller Communication Prioritization.",
+    body: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...",
+  },
+  {
+    date: "Feb 11",
+    title: "Zalora- Seller Communication Prioritization.",
+    body: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...",
+  },
+  {
+    date: "Feb 11",
+    title: "Zalora- Seller Communication Prioritization.",
+    body: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...",
+  },
+  {
+    date: "Feb 11",
+    title: "Zalora- Seller Communication Prioritization.",
+    body: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...",
+  },
+  {
+    date: "Feb 11",
+    title: "Zalora- Seller Communication Prioritization.",
+    body: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...",
+  },
+  {
+    date: "Feb 11",
+    title: "Zalora- Seller Communication Prioritization.",
+    body: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...",
+  },
+  {
+    date: "Feb 11",
+    title: "Zalora- Seller Communication Prioritization.",
+    body: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...",
+  },
+  {
+    date: "Feb 11",
+    title: "Zalora- Seller Communication Prioritization.",
+    body: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...",
+  },
+  {
+    date: "Feb 11",
+    title: "Zalora- Seller Communication Prioritization.",
+    body: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...",
+  },
+  {
+    date: "Feb 11",
+    title: "Zalora- Seller Communication Prioritization.",
+    body: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...",
+  },
+  {
+    date: "Feb 11",
+    title: "Zalora- Seller Communication Prioritization.",
+    body: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...",
+  },
+  {
+    date: "Feb 11",
+    title: "Zalora- Seller Communication Prioritization.",
+    body: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...",
+  },
+  {
+    date: "Feb 11",
+    title: "Zalora- Seller Communication Prioritization.",
+    body: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...",
+  },
+  {
+    date: "Feb 11",
+    title: "Zalora- Seller Communication Prioritization.",
+    body: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...",
+  },
+  {
+    date: "Feb 11",
+    title: "Zalora- Seller Communication Prioritization.",
+    body: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...",
+  },
+  {
+    date: "Feb 11",
+    title: "Zalora- Seller Communication Prioritization.",
+    body: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...",
+  },
+];
