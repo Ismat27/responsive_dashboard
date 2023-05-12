a5fef81f70df37c9d4ad9fc48e56bb731593e899
ismat27 <adegbiteismail27@gmail.com>
2023-05-12 17:26:05 +0100

    update header component for mobile devices

--- src/components/HeaderMobile.tsx
+++ src/components/HeaderMobile.tsx
@@ -1,16 +1,32 @@
-import hamburger from "../assets/menu_bar.png"
+import hamburger from "../assets/menu_bar.png";
+// import searchIcon from "../assets/search.svg";
+import HeaderProfile from "./HeaderProfile";
 
 type PropsType = {
-    setSideTranslate: React.Dispatch<React.SetStateAction<boolean>>,
-    sideTranslated: boolean
-}
+  setSideTranslate: React.Dispatch<React.SetStateAction<boolean>>;
+  sideTranslated: boolean;
+};
 
 const HeaderMobile = ({ setSideTranslate, sideTranslated }: PropsType) => {
   return (
     <header className="bg-black-100 h-[62px] grid items-center lg:hidden">
-        <img onClick={() => setSideTranslate(!sideTranslated)} src={hamburger} alt="side bar toggler" />
+      <div className="flex justify-between items-center px-4 sm:px-4">
+        <div className="flex start gap-2">
+          <img
+            onClick={() => setSideTranslate(!sideTranslated)}
+            src={hamburger}
+            alt="side bar toggler"
+            role="button"
+          />
+          <div className="flex items-start">
+            {/* <img role="button" alt="search icon" src={searchIcon} /> */}
+            {/* <input className="w-full" /> */}
+          </div>
+        </div>
+        <HeaderProfile dark={true} />
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default HeaderMobile
+export default HeaderMobile;
