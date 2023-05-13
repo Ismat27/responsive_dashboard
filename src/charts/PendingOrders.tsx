4d9b46dbd505aa99d6c0dc6ae500a714c3bf287f
ismat27 <adegbiteismail27@gmail.com>
2023-05-13 18:48:52 +0100

    chart component for pending order statistics

---
+++ src/charts/PendingOrders.tsx
@@ -0,0 +1,82 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  width: "100%",
+  height: "200px",
+  plugins: {
+    legend: {
+      display: false,
+      position: "bottom" as const,
+      labels: {
+        useBorderRadius: true,
+        boxWidth: 9,
+        boxHeight: 9,
+        borderRadius: 5,
+      },
+    },
+  },
+  scales: {
+    x: {
+      grid: {
+        display: false,
+      },
+    },
+    y: {
+      display: false,
+    },
+  },
+};
+
+const labels = ["T", "W", "T", "F"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "This Week",
+      data: [1, 1, 1, 10],
+      borderColor: "#7367F0",
+      backgroundColor: "#7367F0",
+      barThickness: 8,
+      borderRadius: 8,
+    },
+  ],
+};
+
+const PendingOrders = () => {
+  return (
+    <article className="bg-white rounded-lg p-4 h-full grid grid-rows-[auto_1fr_auto]">
+      <h2 className="text-sm font-bold text-black-200 mb-1">
+        Total Pendinding Order
+      </h2>
+      <div className="self-center relative h-[150px] w-[150px] max-w-[100%] mx-[auto]">
+        <Bar data={data} options={{ ...options }} />
+      </div>
+      <div>
+        <p className="text-sm text-black-100 font-bold">Current Daily Order</p>
+      </div>
+    </article>
+  );
+};
+
+export default PendingOrders;
